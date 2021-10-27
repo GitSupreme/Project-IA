b6afefac16eb1064e734924f329787d3146682cf
Amakye Jephtha <amakyejephtha@gmail.com>
2021-10-27 14:45:20 +0000

    changed text on button

---
+++ Pages/LOGIN.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+
+export default function LogIn(){
+return (
+    <View>
+        <Text>Login to you account</Text>    
+    </View>
+);
+}
