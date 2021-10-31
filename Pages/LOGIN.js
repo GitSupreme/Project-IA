fbac74e6595504fcab331a85c775f57d40c84451
Jephtha 10718928 <amakyejephtha@gmail.com>
2021-10-31 17:13:46 +0000

    Created Login and Welcome pages

--- Pages/LOGIN.js
+++ Pages/LOGIN.js
@@ -1,10 +1,24 @@
 import React from 'react';
-import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import {NavigationContainer} from '@react-navigation/native';
+import {StyleSheet, Text, View } from 'react-native';
 
-export default function LogIn(){
-return (
-    <View>
-        <Text>Login to you account</Text>    
-    </View>
-);
+export default function Login({ navigation }){
+
+    return (
+        <View style={styles.container}>
+            <Text style={{color: 'white'}}>This is the Login page</Text>
+        </View>
+    );
 }
+  
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#000',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  
+});
